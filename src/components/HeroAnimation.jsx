c65709ba4722adf42fe19d29c76539420db75bd8
unknown <farhandotwhizz@gmail.com>
2026-08-14 02:16:35 +0530

    speed of card

--- src/components/HeroAnimation.jsx
+++ src/components/HeroAnimation.jsx
@@ -108,11 +108,11 @@ export default function HeroAnimation({ spotlightImages = [] }) {
           pixel-perfect on the same card sequence.
         */
         .drift-right {
-          animation: driftRightLane 55s linear infinite;
+          animation: driftRightLane 54s linear infinite;
         }
 
         .drift-left {
-          animation: driftLeftLane 55s linear infinite;
+          animation: driftLeftLane 54s linear infinite;
         }
 
         @keyframes driftRightLane {
